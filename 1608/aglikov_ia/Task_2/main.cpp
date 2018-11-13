49730e876fb49a1ac78979c599d564557281aa71
Gachiman <aglikov@bk.ru>
2018-11-13 12:56:23 +0300

    V0.3 root may be != 0

--- 1608/aglikov_ia/Task_2/main.cpp
+++ 1608/aglikov_ia/Task_2/main.cpp
@@ -5,6 +5,7 @@
 #include "mpi.h"
 
 void MPI_Broadcast(void *buf, int count, MPI_Datatype type, int root, MPI_Comm comm);  // Tree
+void MPI_BroadcastDemo(void *buf, int count, MPI_Datatype type, int root, MPI_Comm comm);  // Tree demonstrarion
 
 void MPI_Broadcast0(void *buf, int count, MPI_Datatype type, int root, MPI_Comm comm);  // Not tree
 
@@ -24,19 +25,14 @@ int main(int argc, char* argv[]) {
         else if (procRoot >= ProcNum) procRoot = ProcNum - 1;
     }
 
-    MPI_Broadcast(&procRoot, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_BroadcastDemo(&procRoot, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (ProcRank == procRoot) {
         size = (argc == 3) ? atoi(argv[2]) : 10;
-        std::cout << "Process " << procRoot << " broadcasting data " 
-            << size << std::endl;
         MPI_Broadcast(&size, 1, MPI_INT, procRoot, MPI_COMM_WORLD);
     }
-    else {
+    else
         MPI_Broadcast(&size, 1, MPI_INT, procRoot, MPI_COMM_WORLD);
-        std::cout << "Process " << ProcRank << " received data " << size
-            << " from process " << procRoot << std::endl;
-    }
 
     MPI_Finalize();
     return 0;
@@ -45,12 +41,35 @@ int main(int argc, char* argv[]) {
 void MPI_Broadcast(void *buf, int count, MPI_Datatype type, int root,
                    MPI_Comm comm) {
     int ProcRank, ProcNum;
+    MPI_Status status;
     MPI_Comm_rank(comm, &ProcRank);
     MPI_Comm_size(comm, &ProcNum);
     int procGen = bit(ProcRank);
 
     if (ProcRank != root)
-        MPI_Recv(buf, count, type, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, MPI_STATUS_IGNORE);
+        MPI_Recv(buf, count, type, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &status);
+    else if (root != 0) MPI_Send(buf, count, type, 0, 0, comm);
+
+    for (int i = procGen; i < bit(ProcNum - 1); i++) {
+        if (ProcRank + pow(2, i) >= ProcNum)
+            break;
+        MPI_Send(buf, count, type, ProcRank + pow(2, i), 0, comm);
+    }
+}
+
+void MPI_BroadcastDemo(void *buf, int count, MPI_Datatype type, int root, MPI_Comm comm) {
+    int ProcRank, ProcNum;
+    MPI_Status status;
+    MPI_Comm_rank(comm, &ProcRank);
+    MPI_Comm_size(comm, &ProcNum);
+    int procGen = bit(ProcRank);
+
+    if (ProcRank != root) {
+        MPI_Recv(buf, count, type, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &status);
+        std::cout << "Process " << ProcRank << " received data " << &buf
+            << " from process " << status.MPI_SOURCE << std::endl;
+    }
+    else if (root != 0) MPI_Send(buf, count, type, 0, 0, comm);
 
     for (int i = procGen; i < bit(ProcNum - 1); i++) {
         if (ProcRank + pow(2, i) >= ProcNum)
